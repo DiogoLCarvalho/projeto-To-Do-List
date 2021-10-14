758e94c7d092587399ccaafbfa93e053b191d936
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-10-14 13:21:07 -0300

    Comandos JS Finalizados

--- js/main.js
+++ js/main.js
@@ -9,27 +9,27 @@ inputText.focus();
 
 function addTask() {
     if (inputText.value.trim() === '') {
-        alert('Erro ao digitar nova tarefa, Tente Novamente:');
+        alert('Erro ao digitar nova tarefa, Tente Novamente');
     } else {
         const textLi = inputText.value;
         const li = document.querySelector('.list__item');
         const newTask = li.cloneNode(true);
 
-
+        // Tarefa recebe o texto e manipula atributos
         newTask.querySelector('.label__text').textContent = textLi.toLowerCase();
         newTask.querySelector('#checkbox__').setAttribute('id', `#checkbox__${ul.childElementCount}`);
         newTask.querySelector('#label').setAttribute('for', `#checkbox__${ul.childElementCount}`);
 
-
+        // Remove classes e adiciona no HTML
         newTask.classList.remove('list__item')
         newTask.classList.remove('hide')
         ul.appendChild(newTask);
 
-
+        // Limpa Input
         inputText.value = ''
         inputText.focus();
 
-
+        // Chamando Função que remove tarefa
         newTask.querySelector('.trash').addEventListener('click', function () {
             taskRemove(this);
         });
@@ -44,21 +44,22 @@ function addTask() {
         }
     }
 }
-
+// Adicionar Com Clique
 btnAdd.addEventListener('click', addTask);
 
 // Adicionar Com Tecla Enter
 inputText.addEventListener('keyup', function (e) {
-    var key = e.which || e.keyCode;
+    let key = e.which || e.keyCode;
     if (key == 13) {
         addTask();
     }
 });
 
+// Função que remove
 function taskRemove(parametro) {
     parametro.parentNode.remove(true);
 
-    //Aumentar tamanho do container das tarefas
+    //Diminuindo tamanho do container das tarefas
     if (ul.childElementCount < 15) {
         mainTag.style.height = '100vh';
         for (let i = 0; i < icons.length; i++) {
@@ -67,16 +68,15 @@ function taskRemove(parametro) {
     }
 
     // Pegue o filho de newTask e remova
-
     // Referencia a this como se fosse um target, recebe a tarefa propria; 
     // Isso porque o escopo é o newTask. 
-    // Estão é assim a cada vez que vc aperta o botão ele cria um proprio escobo para aquela unica li;
+    // A cada vez que vc aperta o botão ele cria um proprio escopo para aquela única li;
     // Dentro desta unica li vc pode modificar, é como vc estivesse modiicando o document (q vira newtask)
     // Por conta disso vc pode usar o this ele referencia o elemento q esta sendo criado
 }
 
 
-// ++++++++++++++++++++++++++++++++++++++++++++++
+// Abrir e fechar Terceiro ícone
 let themeIcon = document.querySelector('.icon:nth-of-type(3)');
 let MenuTrueFalse = themeIcon.dataset.aberto;
 
@@ -92,7 +92,7 @@ const ligaDesligaMenu = () => {
 themeIcon.addEventListener('click', ligaDesligaMenu);
 
 
-// ++++++++++++++++++++++++++++++++++++++++++++++
+// Dark Mode e Local Storage
 let htmlTag = document.querySelector('html');
 let darkmode = document.querySelector('input[type="checkbox"]#darkMode');
 
